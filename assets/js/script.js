be8b77a740242ecdc7beb0b58c2f6cfc7b93b10d
Dan-6044 <muriukiduncan60@gmail.com>
2024-10-18 16:55:44 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+
+
+
+function downloadCV() {
+  const cvFileName = 'cv.pdf'; // Replace with the actual file name
+  const cvFilePath = 'assets/' + cvFileName; // Assuming the CV is in the assets folder
+
+  const link = document.createElement('a');
+  link.href = cvFilePath;
+  link.download = cvFileName;
+  link.click();
+}
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+
+
+/**
+ * back to top & header
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
